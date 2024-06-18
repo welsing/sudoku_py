c6d2cfe5ed6bf6588daf0c5ff0e7a64d7737e031
Welsing <bernardowelsing90@gmail.com>
2024-06-18 20:39:40 -0300

    table done

--- table.py
+++ table.py
@@ -4,10 +4,10 @@
 
 ## GERAR TABULEIRO VAZIO 
 def empty_table():
-    table = []
-    for i in range(9):
+    table = []            
+    for i in range(9):   # cria listas de 9 zeros 9 vezes
         line = [0]*9
-        table.append(line)
+        table.append(line)   # adiciona a linha ao tabuleiro
     return table
 
 
@@ -17,19 +17,44 @@ def random_start():
 
 
 def view_table(tab):
-    for n, line in enumerate(tab):
-        for n, col in enumerate(line):
-            if n == 3:
+    line_labels = "ABCDEFGHI"
+    print("="*26)
+    for n, line in enumerate(tab):  # Faz um laço pelo que passa linha por linha do tabuleiro
+
+        if n % 3 == 0 and n != 0:   # Condição para criar separador a cada 3 linhas
+            
+            print("-"*26)
+
+        print(line_labels[n] + "  ", end='')   # Atribui uma letra para cada linha
+
+        for j, col in enumerate(line):  # Passa por cada elemento (coluna) da linha selecionada
+
+            if j % 3 == 0 and j != 0:  # Condição que cria o separador vertical
                 print("| ", end="")
-            if col == 0:
+
+            if col == 0:            # Printa o elemento (col) da linha (line)
                 print(".", end=' ')
             else:
                 print(col, end=' ')
+        print('|', end='')
         print()
 
 
 
-view_table(empty_table())
+    print('-- ', end='')  
+    col_labels = "JKLMNOPQR"    
+    for i in range(len(col_labels)):          # Da nome as colunas
+        print(col_labels[i], end=' ')
+        if i == 2 or i == 5 and i != 0:
+            print('  ', end='')
+    print("|", end='')
+    print()        
+    print("="*26)
+        
 
-# for i, g in enumerate(empty_table()):
-#     print(i+1, g)
+
+tabuleiro = empty_table()
+
+
+view_table(tabuleiro)
+
